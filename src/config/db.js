03f36ffe843f01e3831bd490e6b25291bb17d0d6
Ramiro Fassetta <ramiro.fassetta.01@gmail.com>
2024-08-20 22:10:11 -0300

    cambio de puerto db

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ dotenv.config({ path: ".env" });
 
 const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    port: process.env.PORT,
+    port: 3306,
     dialect: "mysql",
     define: {
         timestamps: true
